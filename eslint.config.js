103204dae1f4857e69b7a296876c79c98a973763
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-05-15 21:31:49 -0600

    fix: pnpm configuration

--- eslint.config.js
+++ eslint.config.js
@@ -6,12 +6,12 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['**/dist/**', 'eslint.config.js']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(() => {
+  return {
+    root: 'playground',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@caeher/react-tiptap-editor': resolve(__dirname, 'src/index.ts'),
+      },
+    },
+    build: {
+      outDir: '../dist-playground',
+      emptyOutDir: true,
+    }
+  };
 });
